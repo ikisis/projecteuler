3888d84c59434d3cd8e8b4dabce7fcffb8d57111
sangwook.park <ikisis@gmail.com>
2018-03-07 09:19:54 +0900

    add solution to p1

--- euler-java/src/net/projecteuler/Problem1.java
+++ euler-java/src/net/projecteuler/Problem1.java
@@ -10,6 +10,35 @@ public class Problem1 {
 
   public static void main(String[] args) {
 
+    brute_force();
+
+    boolean[] checked = new boolean[1001];
+
+    int a = 3, b = 5, sum = 0;
+
+    while (a < 1000) {
+      if (!checked[a]) {
+        checked[a] = true;
+        sum += a;
+      }
+      a += 3;
+
+      if (b < 1000) {
+        if (!checked[b]) {
+          checked[b] = true;
+          sum += b;
+        }
+        b += 5;
+      }
+
+    }
+
+    System.out.println(sum);
+
+  }
+
+  public static void brute_force() {
+
     int sum = 0;
 
     for (int i = 1; i < 1000; i++) {
