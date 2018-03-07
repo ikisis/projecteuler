e185fe263311c7436eeaba8942f507691c450cfe
sangwook.park <ikisis@gmail.com>
2018-03-07 09:04:37 +0900

    solved p5

---
+++ euler-java/src/net/projecteuler/Problem5.java
@@ -0,0 +1,61 @@
+package net.projecteuler;
+
+import net.projecteuler.utils.PrimitiveUtils;
+
+/*
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any
+ * remainder.
+ *
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+public class Problem5 {
+
+  public static void main(String[] args) {
+
+    int[] primes = new int[20];
+
+    for (int i = 2; i < 20; i++) {
+
+      int[] parts = PrimitiveUtils.doPrimeFactorization(i);
+
+      for (int j = 0; j < parts.length; j++) {
+        if (primes[j] < parts[j]) {
+          primes[j] = parts[j];
+        }
+      }
+
+    }
+
+    PrimitiveUtils.prime_factorization_print(primes);
+
+    System.out.println(PrimitiveUtils.reversePrimeFactorization(primes));
+
+    System.out.println(brute_forced());
+  }
+
+  public static int brute_forced() {
+
+    int n = 0;
+    for (int i = 1; i < 1000000000; i++) {
+
+      boolean flag = true;
+
+      for (int j = 1; j <= 20; j++) {
+
+        if (i % j != 0) {
+          flag = false;
+          break;
+        }
+
+      }
+
+      if (flag) {
+        n = i;
+        break;
+      }
+    }
+
+    return n;
+
+  }
+}
---
+++ euler-java/src/net/projecteuler/utils/PrimitiveUtils.java
@@ -0,0 +1,83 @@
+package net.projecteuler.utils;
+
+public class PrimitiveUtils {
+
+  public static int reversePrimeFactorization(int[] primes) {
+
+    int res = 1;
+    for (int i = 0; i < primes.length; i++) {
+      if (primes[i] > 0) {
+        int n = 1;
+        for (int j = 0; j < primes[i]; j++) {
+          n = n * i;
+        }
+
+        res = res * n;
+      }
+    }
+
+    return res;
+  }
+
+  public static int[] doPrimeFactorization(int n) {
+
+    int[] primes = new int[n + 1];
+
+    int acc = n;
+
+    for (int i = 2; i <= acc;) {
+      if (acc % i == 0) {
+        acc = acc / i;
+        primes[i] = primes[i] + 1;
+        i = 2;
+      } else {
+        i++;
+      }
+    }
+
+    return primes;
+  }
+
+  public static int[] primes() {
+
+    int[] primes = new int[20];
+
+    int size = 0;
+    for (int i = 2; i <= 20; i++) {
+      boolean flag = true;
+      for (int j = 2; j < i; j++) {
+        if (i % j == 0) {
+          flag = false;
+          break;
+        }
+      }
+
+      if (flag) {
+        primes[size++] = i;
+      }
+    }
+
+    return primes;
+
+  }
+
+  public static void prime_factorization_print(int[] primes) {
+    boolean flag = true;
+    for (int i = 0; i < primes.length; i++) {
+
+      if (primes[i] != 0) {
+        if (flag) {
+          flag = false;
+        } else {
+          System.out.print(" + ");
+
+        }
+        System.out.print(i);
+        if (primes[i] > 1) {
+          System.out.print("^" + primes[i]);
+        }
+      }
+    }
+    System.out.println();
+  }
+}
