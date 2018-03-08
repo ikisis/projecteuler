cc4429b3d3aa44fb633803de6bce52c50e0e0be7
sangwook.park <ikisis@gmail.com>
2018-03-08 21:28:02 +0900

    solved p15, add print grid to utils

--- euler-java/src/net/projecteuler/Problem0015.java
+++ euler-java/src/net/projecteuler/Problem0015.java
@@ -1,5 +1,7 @@
 package net.projecteuler;
 
+import net.projecteuler.utils.Utils;
+
 /*
  * Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
  * there are exactly 6 routes to the bottom right corner.
@@ -11,6 +13,37 @@ public class Problem0015 {
 
   public static void main(String[] args) {
 
+    Utils.stopwatch(Problem0015::pascals_triangle);
+  }
+
+  @Deprecated
+  public static long pathTo(int n, int m) {
+    if (n == 0 || m == 0) {
+      return 1L;
+    }
+
+    return pathTo(n - 1, m) + pathTo(n, m - 1);
+
+  }
+
+  public static void pascals_triangle() {
+    int n = 21, m = 21;
+
+    long[][] grid = new long[n][m];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i == 0 || j == 0) {
+          grid[i][j] = 1;
+        } else {
+          grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+        }
+
+      }
+    }
+
+    System.out.println(grid[n - 1][m - 1]);
+    // Utils.print_grid(grid);
   }
 
 }
--- euler-java/src/net/projecteuler/utils/Utils.java
+++ euler-java/src/net/projecteuler/utils/Utils.java
@@ -2,6 +2,24 @@ package net.projecteuler.utils;
 
 public class Utils {
 
+  public static void print_grid(long[][] grid) {
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        System.out.print(grid[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void print_grid(int[][] grid) {
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        System.out.print(grid[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
   public static void slowmotion(long term) {
 
     try {
