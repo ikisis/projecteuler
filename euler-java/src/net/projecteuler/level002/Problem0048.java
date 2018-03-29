5488c6c27f48eaac2d12f420029b9acd806a8f5f
sangwook.park <ikisis@gmail.com>
2018-03-30 08:56:25 +0900

    solved p48

---
+++ euler-java/src/net/projecteuler/level002/Problem0048.java
@@ -0,0 +1,37 @@
+package net.projecteuler.level002;
+
+import net.projecteuler.utils.Utils;
+
+/*
+ * The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+ *
+ * Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+ */
+public class Problem0048 {
+
+  public static void main(String[] args) {
+
+    Utils.stopwatch(Problem0048::first_try);
+
+    // System.out.println(Long.MAX_VALUE % 10000000000L);
+  }
+
+  public static void first_try() {
+
+    int limit = 1000;
+
+    long result = 0;
+    for (int n = 1; n <= limit; n++) {
+      long sp = 1;
+      for (int p = 0; p < n; p++) {
+        sp *= n;
+        sp %= 10000000000L;
+      }
+      result += sp;
+    }
+
+    System.out.println(result % 10000000000L);
+
+  }
+
+}
