03549a2f97ec5acd474e271df2f01a1506eb29a6
sangwook.park <ikisis@gmail.com>
2018-04-04 17:31:10 +0900

    solved p52

---
+++ euler-java/src/net/projecteuler/level002/Problem0052.java
@@ -0,0 +1,47 @@
+package net.projecteuler.level002;
+
+import java.util.Arrays;
+
+import net.projecteuler.utils.Utils;
+
+/*
+ * It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits,
+ * but in a different order.
+ *
+ * Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+ */
+public class Problem0052 {
+
+  public static void main(String[] args) {
+    Utils.stopwatch(Problem0052::first_try);
+  }
+
+  public static void first_try() {
+
+    for (int i = 2; i < 1000000; i++) {
+      boolean found = true;
+      String sorted_a = sortedString(i);
+      for (int j = 2; j <= 6; j++) {
+        if (!sorted_a.equals(sortedString(i * j))) {
+          found = false;
+          break;
+        }
+      }
+
+      if (found) {
+        System.out.println(i);
+        break;
+      }
+    }
+
+  }
+
+  public static String sortedString(int i) {
+    char[] chars = Integer.toString(i).toCharArray();
+
+    Arrays.sort(chars);
+
+    return new String(chars);
+  }
+
+}
