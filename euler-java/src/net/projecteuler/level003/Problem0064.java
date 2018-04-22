c3d1cd81e56755281ee3ff5c4d33ae518da9c50a
sangwook.park <ikisis@gmail.com>
2018-04-22 23:27:16 +0900

    prepare p64

---
+++ euler-java/src/net/projecteuler/level003/Problem0064.java
@@ -0,0 +1,20 @@
+package net.projecteuler.level003;
+
+import net.projecteuler.utils.Utils;
+
+/*
+ * How many continued fractions for N ≤ 10000 have an odd period?
+ */
+public class Problem0064 {
+
+  public static void main(String[] args) {
+
+    Utils.stopwatch(Problem0064::first_try);
+
+  }
+
+  public static void first_try() {
+
+  }
+
+}
