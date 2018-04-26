a90c7576a6748906ce514156f4b12dbd67a35aae
sangwook.park <ikisis@gmail.com>
2018-04-26 09:36:12 +0900

    solved p6 with foldLeft

--- euler-scala/src/net/projecteuler/level001/Problem0006.scala
+++ euler-scala/src/net/projecteuler/level001/Problem0006.scala
@@ -12,20 +12,33 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
  */
 object Problem0006 {
-  
+
   def main(args: Array[String]): Unit = {
+
+    solution1
+    solution2
+
+  }
+
+  def solution2 = {
+
+    val (a, b)  = (1 to 100).foldLeft((0,0))((a, b) => (a._1 + b * b, a._2 + b))
+
+    println(b * b - a)
     
+  }
+
+  def solution1 = {
+
     var sum_of_square = 0
     var square_of_sum = 0
-    
-    1.to(100).foreach{ i => 
+
+    (1 to 100).foreach { i =>
       sum_of_square += i * i
       square_of_sum += i
     }
-    
+    println(s"$square_of_sum , $sum_of_square")
     println(s"${square_of_sum * square_of_sum - sum_of_square}")
-    
+
   }
-  
-  
 }
