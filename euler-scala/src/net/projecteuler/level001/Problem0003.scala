f38ec861e2d5e75439a86576d034333f86cdf794
sangwook.park <ikisis@gmail.com>
2018-05-08 23:05:33 +0900

    add loop solution to scala p3

--- euler-scala/src/net/projecteuler/level001/Problem0003.scala
+++ euler-scala/src/net/projecteuler/level001/Problem0003.scala
@@ -3,6 +3,10 @@ package net.projecteuler.level001
 import scala.math.BigInt
 import java.math.BigInteger
 
+import net.projecteuler.utils.Stopwatch
+
+import scala.annotation.tailrec
+
 /*
  * The prime factors of 13195 are 5, 7, 13 and 29.
  *
@@ -10,17 +14,45 @@ import java.math.BigInteger
  */
 object Problem0003 {
   def main(args: Array[String]): Unit = {
-    
+
+    Stopwatch.elapsed(solution)
+
+    Stopwatch.elapsed(solution_loop)
+
+  }
+
+  def solution = {
     val n = 600851475143L
-    
-    def rec(n: Long, d: Long):Long = 
+
+    @tailrec
+    def rec(n: Long, d: Long):Long =
       if(n == d) d
       else {
         if(n % d == 0) rec(n/d, d)
         else rec(n,d + 1)
       }
-      
+
     println(rec(n,2))
-    
   }
+
+  def solution_loop = {
+    var a = 600851475143L
+    var b = 5
+
+    while(a > b) {
+
+      if(a % b == 0) {
+        a /= b
+      } else {
+        b += 1
+      }
+
+    }
+
+    println(b)
+
+
+  }
+
+
 }
