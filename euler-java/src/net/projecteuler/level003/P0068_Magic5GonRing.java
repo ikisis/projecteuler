c9f778820725b3fbc9d34c98be0b870686a843ac
sangwook.park <ikisis@gmail.com>
2020-07-09 08:10:42 +0000

    solved p68

--- euler-java/src/net/projecteuler/level003/P0068_Magic5GonRing.java
+++ euler-java/src/net/projecteuler/level003/P0068_Magic5GonRing.java
@@ -10,6 +10,51 @@ public class P0068_Magic5GonRing {
 
 		Permutation.make(nums, nums.length, seq -> {
 
+			if(seq[5] == 10 || seq[6] == 10 || seq[7] == 10 || seq[8] == 10 || seq[9] == 10) {
+				return;
+			}
+
+			int sum = 0;
+			boolean sameAll = true;
+			int minIndex = 0;
+			int minA = 1098; 
+			for(int i = 0; i < 5; i++) {
+				int a = seq[i];
+				int b = seq[i + 5];
+				int c = 0;
+				if(i < 4) {
+					c = seq[i + 6];
+				} else {
+					c = seq[5];
+				}
+				if(sum > 0 && sum != a + b + c) {
+					sameAll = false;
+					break;
+				}
+				int aa = a * 100 + b * 10 + c;
+				if(minA > aa) {
+					minA = aa;
+					minIndex = i;
+				}
+				sum = a + b + c;
+			}
+			
+			if(sameAll && minA == 653) {
+				System.out.print(minA + "> ");
+				for(int i = 0 ; i < 5; i ++) {
+					int a = seq[i];
+					int b = seq[i + 5];
+					int c = 0;
+					if(i < 4) {
+						c = seq[i + 6];
+					} else {
+						c = seq[5];
+					}
+					System.out.print("[" + a + " " + b + " " + c + "]");
+				}
+				System.out.println();
+			}
+
 		});
 
 	}
